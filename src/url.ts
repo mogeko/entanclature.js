a81e18f92e1a6d84545554c8d92c571656dfa840
Mogeko <zhengjunyi@live.com>
2022-10-20 23:09:49 +0200

    feat: reimplement ExURL using setter and getter

--- src/url.ts
+++ src/url.ts
@@ -1,38 +1,73 @@
 import isEmpty from "./utils/isEmpty";
 
-export class ExURL extends URL {
-  baseURL: string;
-  extension?: string;
-  file?: string;
-  filename?: string;
+const AVAILABLE_EXT = ["avif", "gif", "jpeg", "jpg", "png", "tiff", "webp"] as const;
 
-  constructor(url: string | ExURL) {
+export class ExURL extends URL {
+  private _baseURL: string;
+  private _extension?: Ext;
+  private _filename?: string;
+
+  constructor(url: string) {
     super(url);
 
-    this.baseURL = `${this.protocol}//${this.host}/`;
+    this._baseURL = `${this.protocol}//${this.host}`;
 
     if (this.pathname.length !== 1) {
       const pathList = this.pathname.split("/").filter((s) => !isEmpty(s));
-      this.file = pathList.splice(-1, 1, "").shift();
+      this.file = pathList.pop();
       this.baseURL += pathList.join("/");
-      if (this.file) {
-        const [filename, ext] = this.file.split(".");
-        this.filename = filename;
-        this.extension = ext;
-      }
     }
   }
+
+  public set baseURL(url: string) {
+    this._baseURL = url;
+  }
+
+  public get baseURL() {
+    return this._baseURL.endsWith("/") ? this._baseURL : this._baseURL + "/";
+  }
+
+  public set extension(ext: Ext | undefined) {
+    if (!ext) return;
+    if (AVAILABLE_EXT.includes(ext)) {
+      this._extension = ext;
+    } else throw URIError(`We don't support .${ext} files yet.`);
+  }
+
+  public get extension() {
+    return this._extension;
+  }
+
+  public set file(file: string | undefined) {
+    const [filename, ext] = (file ?? "").split(".");
+    this.filename = filename;
+    this.extension = ext as Ext;
+  }
+
+  public get file() {
+    return [this.filename, this.extension].filter((s) => !isEmpty(s)).join(".");
+  }
+
+  public set filename(name: string | undefined) {
+    this._filename = name;
+  }
+
+  public get filename() {
+    return this._filename;
+  }
 }
+
+type Ext = typeof AVAILABLE_EXT[number];
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
 
   it("ExURL", () => {
-    const url = new ExURL("https://example.com/with/some/files.png/");
+    const url = new ExURL("https://example.com/with/some/files");
 
     expect(url.baseURL).toEqual("https://example.com/with/some/");
-    expect(url.file).toEqual("files.png");
+    expect(url.file).toEqual("files");
     expect(url.filename).toEqual("files");
-    expect(url.extension).toEqual("png");
+    expect(url.extension).toEqual(undefined);
   });
 }
