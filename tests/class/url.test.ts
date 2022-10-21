7d6472a3b6a827144943efe74a1752df2ba7a7ff
Mogeko <zhengjunyi@live.com>
2022-10-21 20:49:48 +0200

    fix(test): fix a import error

--- tests/class/url.test.ts
+++ tests/class/url.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import FileURL from "../../src/class/url";
+import { FileURL } from "../../src/class/url";
 
 import type { MIME } from "../../src/core/grammar";
 
