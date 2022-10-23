00d782b26fe1b0bd5976c9a0abdd7c1f39f98680
Mogeko <zhengjunyi@live.com>
2022-10-23 22:19:19 +0200

    build: ignore the code coverage of the polyfill

--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -1,4 +1,7 @@
+/** Deep cloning of an Object */
 export function clone<T = any>(obj: T): T {
+  /* c8 ignore next 6 */
+  // a polyfill for structuredClone
   if (typeof structuredClone === "undefined") {
     return JSON.parse(JSON.stringify(obj));
   } else {
